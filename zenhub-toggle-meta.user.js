2787906a0595b0e11ccd871446c7bddd9d600175
Rob Garrison <wowmotty@gmail.com>
2016-07-26 13:08:25 -0500

    Toggle Meta: Update for new boards layout

--- zenhub-toggle-meta.user.js
+++ zenhub-toggle-meta.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          ZenHub Toggle Meta
-// @version       0.1.4
+// @version       0.1.5
 // @description   Toggle pipeline issue meta data (labels)
 // @license       https://creativecommons.org/licenses/by-sa/4.0/
 // @namespace     http://github.com/Mottie
@@ -12,75 +12,81 @@
 // @downloadURL   https://raw.githubusercontent.com/Mottie/Zenhub-userscripts/master/zenhub-toggle-meta.user.js
 // ==/UserScript==
 /* global GM_addStyle */
-(function() {
+/* jshint esnext:true, unused:true */
+(() => {
   "use strict";
-  var busy = false,
-  isVisible = true,
+  let busy = false,
+    isVisible = true,
 
-  markup = [
-    "<div class='zh-board-menu-item'>",
-      "<button id='zhu-toggle-meta-button' class='zh-toggle-button tooltipped tooltipped-n selected' aria-label='Toggle Issue Meta Data'>",
-        "<span class='zhu-toggle-meta'>",
-          "<svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 15 15'>",
-            "<path fill='none' stroke='currentColor' stroke-linecap='round' d='M1.972 1.156H6.1c.279.027.788.322 1.005.497l6.618 6.469c.206.2.197.937 0 1.149l-4.581 4.537c-.196.164-.848.18-1.03 0l-6.669-6.73c-.186-.194-.316-.751-.318-1.017V2.096c.015-.314.529-.926.847-.94z'/>",
-            "<circle fill='none' stroke='currentColor' cx='4.8' cy='4.6' r='1'/>",
-          "</svg>",
-        "</span>",
-      "</button>",
-    "</div>"
-  ].join(""),
+    $style = document.createElement("style"),
 
-  addButton = function() {
+    markup = `
+      <button id="zhu-toggle-meta-button" class="zhc-toggle-button tooltipped tooltipped-n zhc-toggle-button--is-active" aria-label="Toggle Issue Meta Data">
+        <span class="zhu-toggle-meta zhc-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" d="M1.972 1.156H6.1c.279.027.788.322 1.005.497l6.618 6.469c.206.2.197.937 0 1.149l-4.581 4.537c-.196.164-.848.18-1.03 0l-6.669-6.73c-.186-.194-.316-.751-.318-1.017V2.096c.015-.314.529-.926.847-.94z"/>
+            <circle fill="none" stroke="currentColor" cx="4.8" cy="4.6" r="1"/>
+          </svg>
+        </span>
+      </button>
+    `;
+
+  function addButton() {
     // set busy flag to ignore DOMNodeInserted firing while adding new content
     busy = true;
-    var el,
-      ref = document.querySelector("#zh-show-closed-pipeline");
+    let el,
+      ref = $(".zhc-search-bar");
     // look for ZenHub pipeline
-    if (ref && !document.querySelector(".zhu-toggle-meta")) {
-      el = document.createElement("li");
+    if (ref && !$("#zhu-toggle-meta-button")) {
+      el = document.createElement("div");
       el.id = "zhu-toggle-meta";
-      el.className = "zh-board-menu-itemgroup";
+      el.className = "zhc-tooltip zhc-tooltip--top";
       el.innerHTML = markup;
-      ref.parentNode.insertBefore(el, ref.nextSibling);
+      ref.parentNode.insertBefore(el, ref);
 
-      el.addEventListener("click", function(){
+      el.addEventListener("click", () => {
         isVisible = !isVisible;
         toggleLabels();
         document.activeElement.blur();
       });
     }
     busy = false;
-  },
+  }
 
-  toggleLabels = function() {
-    var meta = document.querySelectorAll(".zh-issuecard-meta"),
-      indx = meta.length,
-      button = document.querySelector("#zhu-toggle-meta-button");
-    while (indx--) {
-      meta[indx].style.display = isVisible ? '' : 'none';
-    }
+  function toggleLabels() {
+    let button = $("#zhu-toggle-meta-button");
     if (isVisible) {
-      button.classList.add("selected");
+      button.classList.add("zhc-toggle-button--is-active");
+      $style.disabled = true;
     } else {
-      button.classList.remove("selected");
+      button.classList.remove("zhc-toggle-button--is-active");
+      $style.disabled = false;
     }
-  },
+  }
 
-  init = function(){
-    GM_addStyle([
-      "#zhu-toggle-meta-button { padding-top: 6px; }",
-      "#zhu-toggle-meta-button svg { color: #000; }"
-    ].join(""));
+  function $(selector) {
+    return document.querySelector(selector);
+  }
 
-    document.body.addEventListener("DOMNodeInserted", function(event) {
-      if (!busy && document.querySelector(".zh-board")) {
+  function init() {
+    GM_addStyle(`
+      #zhu-toggle-meta-button .zhc-icon { padding-top: 2px; }
+      #zhu-toggle-meta-button svg { color: #000; }
+    `);
+
+    document.body.appendChild($style);
+    $style.textContent = ".zhc-issue-card__meta { display: none; }";
+    $style.disabled = true;
+
+    document.body.addEventListener("DOMNodeInserted", () => {
+      if (!busy && $(".zh-board")) {
         addButton();
       }
     });
-  };
+  }
 
   // initialize if ZenHub active
-  if (document.querySelector(".zhio, .zhe")) {
+  if ($(".zhio, .zhe")) {
     init();
   }
 
